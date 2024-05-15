5bb6cdeba85c2ae3e2df7629229a0a637b09ea72
robamoadi <148704871+robamoadi@users.noreply.github.com>
2024-05-16 00:55:49 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+
+  //component data
+  state ={
+    name: 'Roba ',
+    age : 27
+  }
+
+   //function render
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
-      <h2>Hellow react</h2>
+      <h2>Hellow {this.state.name}</h2>
+      <h2>Age : {this.state.age}</h2>
       </header>
     </div>
-  );
-}
+    )
+  } 
+};
 
 export default App;
