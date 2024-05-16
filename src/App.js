8e93d53d2f3397fd27890db3e69fe774a12ed3ca
robamoadi <148704871+robamoadi@users.noreply.github.com>
2024-05-16 14:50:12 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Car from './components/Car'
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Garage</h1>
+      <Car />
       </header>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/Car.js
@@ -0,0 +1,15 @@
+import React , {Component} from "react";
+
+class Car extends Component{
+    render(){
+        return(
+            <div style={{color : 'lightblue'}}>
+            <p>Brand : HUNDA</p>
+            <p>Color : red</p>
+            <p>Modal : CIVIC</p>
+            <p>Year : 2024 </p>
+            </div>
+        )
+    }
+}
+export default Car
