874129773bb9fdee74dc0aa5220bcb8b08858e49
robamoadi <148704871+robamoadi@users.noreply.github.com>
2024-05-16 14:11:48 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
 
   //component data
   state ={
-    name: 'Roba ',
+    userNamee: 'Roba ',
+    email : 'roba123@gmail.com',
     age : 27
+  }
+
+  hanndleNameChange = (e) =>{
+    this.setState({[e.target.name] : e.target.value})
+    //this.setState({userNamee : e.target.value})
+    //this.setState({email : e.target.value})
+    //this.setState({age : e.target.value})
   }
 
    //function render
@@ -15,8 +22,14 @@ class App extends React.Component {
     return (
     <div className="App">
       <header className="App-header">
-      <h2>Hellow {this.state.name}</h2>
-      <h2>Age : {this.state.age}</h2>
+      <h2>Hellow {this.state.userNamee}</h2>
+      <h4>Age : {this.state.age}</h4>
+      <h4>Email : {this.state.email}</h4>
+      <button onClick={() => this.setState({age: ++this.state.age})}>Add age</button>
+      <button onClick={() => this.setState({age: --this.state.age})}>Sub age</button>
+      <input type='text' name='userNamee' value={this.state.userNamee} onChange={this.hanndleNameChange}></input>
+      <input type='number' name='age' value={this.state.age} onChange={this.hanndleNameChange}></input>
+      <input type='text' name='email' value={this.state.email} onChange={this.hanndleNameChange}></input>
       </header>
     </div>
     )
