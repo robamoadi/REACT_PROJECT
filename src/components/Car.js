f8b30eb8a615637331d10e6eda41303772fc0bac
robamoadi <148704871+robamoadi@users.noreply.github.com>
2024-05-17 19:48:44 +0300

    .

--- src/components/Car.js
+++ src/components/Car.js
@@ -10,7 +10,7 @@ class Car extends Component {
                 <p>Brand : {brand}</p>
                 <p>Color : {color}</p>
                 <p>Modal : {modal}</p>
-                <p>Year : {year} </p><button style={{color}} onClick={() => {addYear()}}>add year</button>
+                <p>Year : {year} </p><button style={{color}} onClick={() => {}}>add year</button>
                 <p>Coast : {coast} </p>
                 <button style={{color: 'red'}} onClick={() => {deleteCar(id)}}>x</button>
             <hr></hr>
